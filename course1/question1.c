83e599b3f70b96d08f539d9d2e5bd5aa4e2debe0
Mason <mason9003122001@gmail.com>
2024-03-06 22:12:18 +0800

    try what it happened if condition in forLoop is n = 1

--- course1/question1.c
+++ course1/question1.c
@@ -3,9 +3,11 @@
 int main(){
     int k = 0;
     int i = 0;
-    for(i = 0,k = 0; k = 0; i++, k++){
+    for(i = 0,k = 0; k = 2; i++, k++){
         printf("%d",k);
+        if(k == 2)break;
         k++;
+        if(k == 2)break;
     }
     return 0;
     // how many rounds does this loop run?(or how many times would this loop print k?)
