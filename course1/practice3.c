f6f369d4171175d481e6355d35ef43db0c58a489
Mason <mason9003122001@gmail.com>
2024-03-07 00:12:06 +0800

    fix typo

--- course1/practice3.c
+++ course1/practice3.c
@@ -3,7 +3,7 @@
 int main(){
     int temp = 1;
     int input = 0;
-    printf("please input a interger and I will compute factorial.");
+    printf("please input a integer and I will compute factorial.");
     scanf("%d",&input);
     for(int i = 1; i<(input+1); i++){
         temp = temp * i;
