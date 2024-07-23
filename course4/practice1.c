3b94d5ce7cd2273ff50a2a96889d0b1ed325f5ee
MasonZheng <mason9003122001@gmail.com>
2024-07-23 22:31:55 +0800

    add new case to explain how constant char pointer work

--- course4/practice1.c
+++ course4/practice1.c
@@ -7,15 +7,34 @@ int main(){
     printf("pt_ch is %c\n",ch);
 
     char ch_arr[] = "rabbit";
-    char* pt_chArr = ch_arr; // name of array is the first element address of this array 
+    char* pt_chArr = ch_arr; // name of array is the first element address of this array.
     printf("ch array = %s\n", ch_arr); 
     printf("ch array = %s\n", pt_chArr);
 
-    // or we can also use this form to init a char pointer which is a constant
+    // or we can also use this form to init a char pointer which is a constant.
     char* pt = "abcde";
     printf("pt = %s\n",pt);
+    // *pt = "w"; Notice this syntax.
+    // In some compiler, this bad syntax will build successfully.
+    // but it isn't a legal form, why??
+    // because as we just said, use pointer to initial a char array means it is a "constant",
+    // then as the definition of constant, it shouldn't been changed.
+    // so we should do more correctly to pretend our code from this.
+    // const char* pt = "abcde"; 
 
-    // *pt = "w";
-    // printf("pt = %s\n",pt);
+    /*******************************************************************************************************************/
+    //let's consider this case :
+    char test1[] = "abcde";
+    char test2[] = "alert";
+    const char* result1 = (test1 == test2 ? "yes":"no");
+    printf("if first address equal? Ans: %s\n",result1);
+    // but if in this form :
+    const char* test_pt1 = "abcde";
+    const char *test_pt2 = "abcde";
+    const char* result2 = (test_pt1 == test_pt2 ? "yes":"no");
+    printf("if first address equal? Ans: %s\n",result2);
+    // because of test_pt1, test_pt1 are both "constant", same value "abcde" doesn't need two different space to restore.
+
+    /*******************************************************************************************************************/
     return 0;
 }
