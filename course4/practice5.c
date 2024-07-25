530c617222f33a293e480d1aec042e8f9ed446d4
MasonZheng <mason9003122001@gmail.com>
2024-07-25 23:54:49 +0800

    address of function

---
+++ course4/practice5.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+int Add(int x, int y){
+    return x + y;
+}
+
+int main(){
+    int a = 3;
+    int b = 5;
+    int answer = Add(a,b);
+
+    // what is the address of function?
+
+    printf("%p\n",&Add);
+    printf("%p\n",Add);
+    // both correct
+
+    // but how should we create a pointer parameter for function?
+    int (*p)(int,int) = Add;
+    
+     
+    return 0;
+}
