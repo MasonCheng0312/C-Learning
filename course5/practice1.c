4cff10acb57f00667b4088d9216d80d556c9cb12
MasonZheng <mason9003122001@gmail.com>
2024-08-10 18:54:49 +0800

    try create my_strlen()

--- course5/practice1.c
+++ course5/practice1.c
@@ -1,14 +1,31 @@
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
+
+int my_strlen(char* input){
+    int count = 0;
+    assert(input != NULL);
+    while (*input){
+        count++;
+        input++;
+    }
+    return count;   
+}
+
 
 int main(){
     int str_len = strlen("abcde");
     printf("use strlen lib function for const. str abcde \n");
     printf("result is %d \n",str_len);
     // the principle of strlen() is to find "\0" and count element before "\0"(no include)
-    char str_arr[] ={'a','b','c'};
+
+    char str_arr[] ={'a', 'b', 'c', 'd', 'e', '\0'};
     printf("use strlen lib function for str array with out '\\0'\n");
     printf("result is %d \n",strlen(str_arr)); 
-    printf("this code is wrong and answer is random\n");
+    printf("this code is wrong and answer is random(can't init str array without \\0)\n");
+
+    printf("use my_strlen() build ourself\n");
+    printf("result is %d \n",my_strlen(str_arr));
+
     return 0;
 }
