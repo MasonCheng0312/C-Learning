1dbdb7ab9c756c772d34031f2239ec968bb3d644
MasonZheng <mason9003122001@gmail.com>
2024-08-11 14:01:33 +0800

    create my_strcpy() with a basic expression

--- course5/practice2.c
+++ course5/practice2.c
@@ -2,12 +2,28 @@
 #include <stdio.h>
 #include <assert.h>
 
+char* my_strcpy(char* destination, char* source){
+    char* ret = destination;
+
+    while (*source != '\0')
+    {
+        *destination = *source;
+        destination++;
+        source++;
+    }
+    *destination = *source; // copy '\0'
+
+    return ret;
+}
+
 
 int main(){
     char example_strArray[] = "ohanabi";
     char example_copy[] = "ahoy";
-    strcpy(example_strArray,example_copy); // first argument is space, second one is for str we want to copy
+    char* answer = strcpy(example_strArray,example_copy); // first argument is space, second one is for str we want to copy
     // space must be big enough for str!
-    printf("now strArray is %s\n",example_strArray);
+    printf("now strArray is %s\n",answer);
+
+    printf("now create my_strcpy()!\n");
     return 0;
 }
