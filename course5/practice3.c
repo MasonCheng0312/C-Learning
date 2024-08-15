c2192c887860f96407fb273fb7e465ed4b14c067
MasonZheng <mason9003122001@gmail.com>
2024-08-15 11:28:45 +0800

    create my_strcat()

--- course5/practice3.c
+++ course5/practice3.c
@@ -1,11 +1,23 @@
 #include <string.h>
 #include <stdio.h>
 
+void my_strcat(char* dest, char* source){
+    while (*dest)
+    {
+        dest++;
+    }
+    while(*dest++ = *source++){
+        ;
+    }
+}
+
+
 
 int main(){
-    char str1[6] = "Hello!";
+    char str1[12] = "Hello!";
     char str2[] = "world";
-    strcat(str1,str2);
+    // strcat(str1,str2);
+    my_strcat(str1,str2);
     printf("%s",str1);
     // in older patch c IDE, this might be error because of space of str1 doesn't enough
     // the most conservative method is to init a bigger array and contain "\0" in it
