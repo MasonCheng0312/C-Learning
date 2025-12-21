f95f85e9bdcc5ef742aa2e3255c668eeb84cf187
MasonZheng <mason9003122001@gmail.com>
2025-12-21 15:13:54 +0800

    optimize code from O(N^2) to O(N)

--- self_practice/Leetcode_283.c
+++ self_practice/Leetcode_283.c
@@ -3,8 +3,9 @@ Given an integer array nums, move all 0's to the end of it while maintaining the
 
 Note that you must do this in-place without making a copy of the array.*/
 
+#include <stdio.h>
 
-void moveZeroes(int* nums, int numsSize) {
+void moveZeroes1(int* nums, int numsSize) {
     for(int i = 0;i < numsSize; i ++){
         int* slow_pointer = nums + i ;
         int* fast_pointer = slow_pointer;
@@ -20,3 +21,27 @@ void moveZeroes(int* nums, int numsSize) {
     }
 } // O(N^2)的作法
 
+void moveZeroes2(int* nums, int numsSize) {
+    int* end = nums + numsSize;
+    int* slow = nums;
+    int* fast = nums;
+    for(;fast < end; fast++){
+        if (*fast != 0){
+            int temp = *slow;
+            *slow = *fast;
+            *fast = temp;
+            slow++;
+        }
+    }
+} // O(N)的作法
+
+int main(){
+    int nums[] ={0,1,0,3,12};
+    int size = 5;
+    moveZeroes2(nums,size);
+
+    for(int i = 0;i < size;i++){
+        printf("result = %d\n",nums[i]);
+    }
+    
+}
